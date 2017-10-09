3759b067e9323f24d6e46a49e175bf866fabeae7
xinni <xinni@buffalo.edu>
2017-10-09 01:38:16 -0400

    fit to the brightness control board

--- Left_Light_Control/Left_Light_Control.ino
+++ Left_Light_Control/Left_Light_Control.ino
@@ -5,14 +5,17 @@
 #include "RF24.h"
 #include "SPI.h"
 
-//定义第一个灯板的三个继电器接脚分别为 2，3，4
-#define RELAY1 2
-#define RELAY2 3
-#define RELAY3 4
-//定义第二个灯板的三个继电器接脚分别为 5，6，7
+//定义第一个灯板亮度控制的三位分别为 D2，D3，D4，开关位为D5
+#define L1D0 2
+#define L1D1 3
+#define L1D2 4
+#define L1Switch 5
+//定义第二个灯板亮度控制的三位分别为 D6，D7，D8，开关位为A2
 #define RELAY4 5
-#define RELAY5 6
-#define RELAY6 7
+#define L2D0 6
+#define L2D1 7
+#define L2D2 8
+#define L2Switch A2
 
 RF24 radio(9,10);
 const uint64_t pipe = 0xE6E6E6E6E601;
@@ -25,9 +28,14 @@ void setup() {
   radio.begin();
   radio.openReadingPipe(1, pipe);
   radio.startListening();
-  pinMode(RELAY1, OUTPUT);
-  pinMode(RELAY2, OUTPUT);
-  pinMode(RELAY3, OUTPUT);
+  pinMode(L1D0, OUTPUT);
+  pinMode(L1D1, OUTPUT);
+  pinMode(L1D2, OUTPUT);
+  pinMode(L1Switch, OUTPUT);
+  pinMode(L2D0, OUTPUT);
+  pinMode(L2D1, OUTPUT);
+  pinMode(L2D2, OUTPUT);
+  pinMode(L2Switch, OUTPUT);
 }
 
 void loop() {
@@ -51,40 +59,44 @@ void loop() {
 
 void TurnOn(int num, int brightness) {
   if (num == 1) {
-    if (brightness == 1){
-    //1级亮度
-      digitalWrite(RELAY1, HIGH);
-      digitalWrite(RELAY2, LOW);
-      digitalWrite(RELAY3, LOW);
-    } else if (brightness == 2) {
-      //2级亮度
-      digitalWrite(RELAY1, HIGH);
-      digitalWrite(RELAY2, HIGH);
-      digitalWrite(RELAY3, LOW);
-    } else if (brightness == 3) {
-      //3级亮度
-      digitalWrite(RELAY1, HIGH);
-      digitalWrite(RELAY2, HIGH);
-      digitalWrite(RELAY3, HIGH);
-    } else ;
+    if (brightness >= 0 & brightness <= 7){
+      digitalWrite(L1Switch, LOW);
+      if (brightness % 2) {
+        digitalWrite(L1D0, HIGH);
+      } else {
+        digitalWrite(L1D0, LOW);
+      }
+      if ((brightness/2) % 2) {
+        digitalWrite(L1D1, HIGH);
+      } else {
+        digitalWrite(L1D1, LOW);
+      }
+      if (((brightness/2)/2) % 2) {
+        digitalWrite(L1D2, HIGH);
+      } else {
+        digitalWrite(L1D2, LOW);
+      }
+    } else { }
     msg = "";
   } else if (num == 2) {
-    if (brightness == 1){
-      //1级亮度
-      digitalWrite(RELAY1, HIGH);
-      digitalWrite(RELAY2, LOW);
-      digitalWrite(RELAY3, LOW);
-    } else if (brightness == 2) {
-      //2级亮度
-      digitalWrite(RELAY1, HIGH);
-      digitalWrite(RELAY2, HIGH);
-      digitalWrite(RELAY3, LOW);
-    } else if (brightness == 3) {
-      //3级亮度
-      digitalWrite(RELAY1, HIGH);
-      digitalWrite(RELAY2, HIGH);
-      digitalWrite(RELAY3, HIGH);
-    } else ;
+    if (brightness >= 0 & brightness <= 7){
+      digitalWrite(L1Switch, LOW);
+      if (brightness % 2) {
+        digitalWrite(L1D0, HIGH);
+      } else {
+        digitalWrite(L1D0, LOW);
+      }
+      if ((brightness/2) % 2) {
+        digitalWrite(L1D1, HIGH);
+      } else {
+        digitalWrite(L1D1, LOW);
+      }
+      if (((brightness/2)/2) % 2) {
+        digitalWrite(L1D2, HIGH);
+      } else {
+        digitalWrite(L1D2, LOW);
+      }
+    } else { }
     msg = "";
   } else msg = "";
 }
@@ -92,15 +104,17 @@ void TurnOn(int num, int brightness) {
 void TurnOff(int num) {
   if (num == 1) {
     //关闭所有继电器
-    digitalWrite(RELAY1, LOW);
-    digitalWrite(RELAY2, LOW);
-    digitalWrite(RELAY3, LOW);
+    digitalWrite(L1D0, LOW);
+    digitalWrite(L1D1, LOW);
+    digitalWrite(L1D2, LOW);
+    digitalWrite(L1Switch, HIGH);
     msg = "";
   } else if (num == 2) {
     //关闭所有继电器
-    digitalWrite(RELAY4, LOW);
-    digitalWrite(RELAY5, LOW);
-    digitalWrite(RELAY6, LOW);
+    digitalWrite(L2D0, LOW);
+    digitalWrite(L2D1, LOW);
+    digitalWrite(L2D2, LOW);
+    digitalWrite(L2Switch, HIGH);
     msg = "";
   } else msg = "";
 
